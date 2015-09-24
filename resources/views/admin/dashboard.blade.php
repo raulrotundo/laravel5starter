0803ce73c0c2302ec1733717289dfc2eecfc8fa9
Raul Rotundo <raulrotundo@gmail.com>
2015-09-24 16:17:30 -0300

    delete first and last name, we just have name

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.layouts.master')
 @section('title', 'Dashboard')
 @section('content')
-	Welcome <?=$first_name?> <?=$last_name?>!
+	Welcome {!! $name !!}!
 @endsection
--- resources/views/admin/partials/master/sidebar.blade.php
+++ resources/views/admin/partials/master/sidebar.blade.php
@@ -10,7 +10,7 @@
                 <img src="{{ asset("/public/assets/admin/admin-lte/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image" />
             </div>
             <div class="pull-left info">
-                <p>{!! Auth::user()->first_name !!} {!! Auth::user()->last_name !!}</p>
+                <p>{!! Auth::user()->name !!}</p>
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
