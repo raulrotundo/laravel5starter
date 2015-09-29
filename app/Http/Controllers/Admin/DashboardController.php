6b39ef072be64a16b8d9ea8aefc646d182844f7b
raul_personal_laptop <raulrotundo@gmail.com>
2015-09-28 21:51:02 -0300

    Resolving more conflicts

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -18,11 +18,7 @@ class DashboardController extends Controller
     public function index()
     {
         return view('admin/dashboard', [
-<<<<<<< HEAD
             'name'    => Auth::user()->name
-=======
-            'name' => Auth::user()->name
->>>>>>> 317455b974d439fbc434032fc00e68c3e31a8719
         ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,5 @@
 @extends('admin.layouts.master')
 @section('title', 'Dashboard')
 @section('content')
-<<<<<<< HEAD
-	Welcome <?=$name?>
-=======
 	Welcome {!! $name !!}!
->>>>>>> 317455b974d439fbc434032fc00e68c3e31a8719
 @endsection
