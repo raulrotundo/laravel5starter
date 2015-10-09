f2ed217a8926bfa89e0621647262d6499575653b
Raul Rotundo <raulrotundo@gmail.com>
2015-10-09 16:58:29 -0300

    resolve conflict

---
+++ resources/lang/es/admin/dashboard.php
@@ -0,0 +1,19 @@
+<?php
+return [    
+    'menu' => [
+        'dashboard' => 'Escritorio',
+        'create'    => 'Crear',
+        'show'      => 'Mostrar',
+        'administration' => [
+            'main_title'  => 'Administración',
+            'users'       => 'Usuarios',
+            'roles'       => 'Roles',
+            'permissions' => 'Permisos',            
+        ]
+    ],
+    'welcome' => 'Bienvenido :user!',
+    'profile' => 'Ver mi Perfil',
+    'logout' => 'Cerrar Sesión',
+    'member_since' => 'Miembro desde :member_date',
+    'select_lang' => 'Cambie su idioma:'
+];
