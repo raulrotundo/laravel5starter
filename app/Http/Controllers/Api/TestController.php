d1c647ea2d6409b16cfe42d3f5ec7be585bb60f7
raul_personal_laptop <raulrotundo@gmail.com>
2015-10-31 12:20:01 -0300

    API Login

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -22,4 +22,70 @@ class TestController extends Controller
         $users['users'] = User::all();
         return response()->json(['status'=>'ok','response'=>$users], 200);
     }
+
+    /*
+     * Login API
+     * @return Response
+     */
+    public function login(Request $request)
+    {
+        header('Access-Control-Allow-Origin: *');
+
+        $error_msg = array();
+
+        $rules = array(
+            'email' => 'required',
+            'password' => 'required',
+        ); 
+
+        $validator = Validator::make(Input::all(),$rules);
+
+        if($validator->fails()){
+            array_push($error_msg, $validator->errors());
+        }
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        $throttles = $this->isUsingThrottlesLoginsTrait();
+
+        if ($throttles && $this->hasTooManyLoginAttempts($request)) {
+            $seconds = app(RateLimiter::class)->availableIn(
+                $request->input($this->loginUsername()).$request->ip()
+            );
+            $this->sendLockoutResponse($request);
+            array_push($error_msg, ['message' => $this->getLockoutErrorMessage($seconds)]);
+        }
+
+        //Validate whether the input is email, if yes Auth::attempt using email, else attempt using username
+        $field = filter_var($request->input('email'), FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        $request->merge([$field => $request->input('email')]);
+
+        if (!$error_msg){
+            if (Auth::once($request->only($field, 'password'), $request->has('remember'))) {
+                    $response = Auth::user();                    
+                    $response->SocialData; //Social data info
+                    $response['session'] = ['data' => $request->session()->all(), 'id' => Session::getId()];
+            } else {
+                // If the login attempt was unsuccessful we will increment the number of attempts
+                // to login and redirect the user back to the login form. Of course, when this
+                // user surpasses their maximum number of attempts they will get locked out.
+                if ($throttles) $this->incrementLoginAttempts($request);
+                array_push($error_msg, ['message' => $this->getFailedLoginMessage()]);
+            }
+        }
+
+        $render_response = [
+            'entry'     => $request->all(),
+            'response'  => isset($response) ? $response : null,
+            'status'    => !$error_msg ? 0 : 1,
+            'msg'       => !$error_msg ? "OK" : "ERROR",
+        ];
+
+        if ($error_msg){
+            $render_response['errors'] = array('error' => $error_msg); 
+        }
+
+        return response()->json($render_response);
+    }
 }
